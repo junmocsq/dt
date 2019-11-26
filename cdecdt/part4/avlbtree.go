b4fea2b2d178603ad689b514e7410ec57d788f9a
junmo <junmocsq@163.com>
2019-11-26 19:32:44 +0800

    avl

--- cdecdt/part4/avlbtree.go
+++ cdecdt/part4/avlbtree.go
@@ -1,5 +1,7 @@
 package part4
 
+import "fmt"
+
 // 一棵AVL树是其每个节点的左子树和右子树的高度最多差1的二叉查找树。
 
 type AvlNode struct {
@@ -15,7 +17,66 @@ type Avl struct {
 }
 
 func (this *Avl) Insert(ele int) {
+	if this.Root == nil {
+		this.Root = &AvlNode{Elem: ele}
+		this.Nodenum++
+	} else {
+		this.insert(ele, this.Root, nil, nil)
+	}
+}
 
+func (this *Avl) insert(ele int, node, parent, grandfather *AvlNode) *AvlNode {
+
+	if node == nil {
+		return &AvlNode{Elem: ele}
+	} else if ele == node.Elem {
+		return nil
+	} else if ele < node.Elem {
+		node.Left = this.insert(ele, node.Left, node, parent)
+		if parent != nil && this.height(parent.Left)-this.height(parent.Right) == 2 {
+			if parent.Left.Elem > ele {
+				if grandfather == nil {
+					this.Root = this.rightSingleRotation(parent)
+				} else if grandfather.Left == parent {
+					grandfather.Left = this.rightSingleRotation(parent)
+				} else {
+					grandfather.Right = this.rightSingleRotation(parent)
+				}
+			} else {
+				if grandfather == nil {
+					this.Root = this.leftDoubleRotation(parent)
+				} else if grandfather.Left == parent {
+					grandfather.Left = this.leftDoubleRotation(parent)
+				} else {
+					grandfather.Right = this.leftDoubleRotation(parent)
+				}
+			}
+		}
+
+	} else {
+		node.Right = this.insert(ele, node.Right, node, parent)
+		if parent != nil && this.height(parent.Right)-this.height(parent.Left) == 2 {
+			if parent.Right.Elem < ele {
+				if grandfather == nil {
+					this.Root = this.leftSingleRotation(parent)
+				} else if grandfather.Left == parent {
+					grandfather.Left = this.leftSingleRotation(parent)
+				} else {
+					grandfather.Right = this.leftSingleRotation(parent)
+				}
+			} else {
+				if grandfather == nil {
+					this.Root = this.rightDoubleRotation(parent)
+				} else if grandfather.Left == parent {
+					grandfather.Left = this.rightDoubleRotation(parent)
+				} else {
+					grandfather.Right = this.rightDoubleRotation(parent)
+				}
+			}
+		}
+	}
+	node.Height = this.max(this.height(node.Right), this.height(node.Left)) + 1
+	return node
 }
 
 // 右旋
@@ -23,8 +84,8 @@ func (this *Avl) rightSingleRotation(k2 *AvlNode) *AvlNode {
 	k1 := k2.Right
 	k2.Right = k1.Left
 	k1.Left = k2
-	k2.Height = max(k2.Right.Height, k2.Left.Height)
-	k1.Height = max(k1.Left.Height, k1.Right.Height)
+	k2.Height = this.max(this.height(k2.Right), this.height(k2.Left)) + 1
+	k1.Height = this.max(this.height(k1.Left), this.height(k1.Right)) + 1
 	return k1
 }
 
@@ -33,16 +94,24 @@ func (this *Avl) leftSingleRotation(k2 *AvlNode) *AvlNode {
 	k1 := k2.Left
 	k2.Left = k1.Right
 	k1.Right = k2
-	k2.Height = max(k2.Right.Height, k2.Left.Height)
-	k1.Height = max(k1.Left.Height, k1.Right.Height)
+	k2.Height = this.max(this.height(k2.Right), this.height(k2.Left)) + 1
+	k1.Height = this.max(this.height(k1.Left), this.height(k1.Right)) + 1
 	return k1
 }
 
-func max(a, b int) int {
+func (this *Avl) max(a, b int) int {
 	if a > b {
 		return a
 	} else {
 		return b
+	}
+}
+
+func (this *Avl) height(node *AvlNode) int {
+	if node == nil {
+		return -1
+	} else {
+		return node.Height
 	}
 }
 
@@ -60,4 +129,21 @@ func (this *Avl) rightDoubleRotation(k3 *AvlNode) *AvlNode {
 	k3.Right = this.rightSingleRotation(k3.Right)
 	// 左旋
 	return this.leftSingleRotation(k3)
+}
+
+func (t *Avl) PrintTree() {
+	fmt.Println("tree: 节点个数:", t.Nodenum)
+	t.printtree(t.Root)
+}
+
+func (t *Avl) printtree(node *AvlNode) {
+	if node != nil {
+		fmt.Printf("%v\t", node.Elem)
+		if node.Left != nil {
+			t.printtree(node.Left)
+		}
+		if node.Right != nil {
+			t.printtree(node.Right)
+		}
+	}
 }
--- cdecdt/part4/btree_test.go
+++ cdecdt/part4/btree_test.go
@@ -6,14 +6,22 @@ import (
 )
 
 func TestBinTree(t *testing.T) {
-	tree := NewTree()
-	arr := []int{4, 2, 6, 1, 3, 5, 7}
-	for _, v := range arr {
-		tree.Insert(v)
-	}
-	tree.PrintTree()
-	fmt.Println(tree.Delete(4))
-	fmt.Println(tree.Delete(2))
-	fmt.Println(tree.Delete(6))
-	tree.PrintTree()
+	//tree := NewTree()
+	//arr := []int{4, 2, 6, 1, 3, 5, 7}
+	//for _, v := range arr {
+	//	tree.Insert(v)
+	//}
+	//tree.PrintTree()
+	//fmt.Println(tree.Delete(4))
+	//fmt.Println(tree.Delete(2))
+	//fmt.Println(tree.Delete(6))
+	//tree.PrintTree()
+
+	avl := &Avl{}
+	avl.Insert( 3)
+	avl.Insert( 2)
+	avl.Insert( 1)
+
+	avl.PrintTree()
+	fmt.Println(avl.Root,avl.Root.Left,avl.Root.Right)
 }
